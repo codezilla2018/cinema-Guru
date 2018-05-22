d34b1ea47a56295f78fa0c1889558e3e1e529f00
Lanka <rmllbrathnayaka@gmail.com>
2018-05-22 13:40:22 +0530

    Basic twitter posting script

---
+++ posttwitter.js
@@ -0,0 +1,24 @@
+console.log("started");
+var Twit = require('twit');
+//twitter api configuration keys
+var T = new Twit({
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
+  timeout_ms:           60*1000,  
+  // optional HTTP request timeout to apply to all requests.
+});
+
+var tweet={
+    status:'#travel sri lanka and follow celontravelstyles on instagram'
+}
+T.post('statuses/update',tweet,tweeted);
+
+function tweeted(err,data,response){
+if(err){
+    console.log(err);
+}   else{
+    console.log("it worked");
+    }
+}
