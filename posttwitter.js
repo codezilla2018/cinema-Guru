bc3e53c61ae204c2c655d5005445df28481ded84
Lanka <rmllbrathnayaka@gmail.com>
2018-05-24 03:32:47 +0530

    You tube api

--- posttwitter.js
+++ posttwitter.js
@@ -9,16 +9,20 @@ var T = new Twit({
   timeout_ms:           60*1000,  
   // optional HTTP request timeout to apply to all requests.
 });
-
-var tweet={
-    status:'#travel sri lanka and follow celontravelstyles on instagram'
-}
-T.post('statuses/update',tweet,tweeted);
-
-function tweeted(err,data,response){
-if(err){
-    console.log(err);
-}   else{
-    console.log("it worked");
+//setting up the interval to tweet 1 hour
+setInterval(tweetit, 1000*60*60);
+function tweetit(){
+    var tweet={
+        status:'#travel sri lanka and follow celontravelstyles on instagram'
     }
+    T.post('statuses/update',tweet,tweeted);
+    
+    function tweeted(err,data,response){
+    if(err){
+        console.log(err);
+    }   else{
+        console.log("it worked");
+        }
+    }
+
 }
