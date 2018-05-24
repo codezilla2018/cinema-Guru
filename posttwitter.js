141cfd7565e966528956cb2e43fcdd15b396d550
Lanka <rmllbrathnayaka@gmail.com>
2018-05-24 22:25:55 +0530

    added a array to fectch youtube links

--- posttwitter.js
+++ posttwitter.js
@@ -1,18 +1,12 @@
 var pubSubHubbub = require("pubsubhubbub");
-console.log("started");
 //setting the date
 var date=new Date();
 var Twit = require('twit');
+//setting up the configuration file
+var config=require('./config');
 //twitter api configuration keys
-var T = new Twit({
-    consumer_key:         'BGftu8uaxE6X3EQHuOFUeHpfB',
-    consumer_secret:      '0YCTvhIkSsHvAKFUlspCTZmgrNaejyuCcYzfvH7lQStL6lORqE',
-    access_token:         '997930674907680768-KHft88adacdmVqcOjj05ZuuBzwsU9ly',
-    access_token_secret:  '0kUT31KsrPHNAni62MiL6kMedHQ636i4uvZ7W6pFP2QRo',
-    timeout_ms:           60*1000,  
-  // optional HTTP request timeout to apply to all requests.
-});
-
+var T = new Twit(config);
+console.log("started");
 //receive notifications
 var pubSubSubscriber = pubSubHubbub.createServer('https://cinema2.herokuapp.com');
 var topic = "https://www.youtube.com/xml/feeds/videos.xml?channel_id=UCc2j-7dkYX1vjIKOjCO6zAw";
@@ -32,12 +26,17 @@ pubSubSubscriber.listen(process.env.PORT || 8080);
     });
 });
 
+// fetch the recieved xml file and append the new url to the array 
+    //code come here
+
+url=['https://www.youtube.com/watch?v=ka_5cFW8kMU'];
+
 //setting up the interval to tweet 1 hour
 tweetit();
 setInterval(tweetit, 1000*60*60);
 function tweetit(){
     var tweet={
-        status:'#travel sri lanka posted a new video and follow celontravelstyles on instagram '+ date
+        status:'view the trailer '+url[0]+ date
     }
     T.post('statuses/update',tweet,tweeted);
     
